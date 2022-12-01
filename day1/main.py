a680dbd9628cf2037908ed1ace0f7623c220f127
William Russell <will@wrussell.co.uk>
2022-12-01 21:19:25 +0000

    Remove unused code from day 1

--- day1/main.py
+++ day1/main.py
@@ -1,13 +1,10 @@
 with open('input.txt') as f:
     lines = f.readlines()
-    max_cal = 0
     max_cals = []
     current_sum = 0
 
     for line in lines:
         if line.strip() == "":
-            if current_sum > max_cal:
-                max_cal = current_sum
             max_cals.append(current_sum)
             current_sum = 0
         else:
