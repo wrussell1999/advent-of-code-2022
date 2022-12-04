65f063d110edd91676a1c1549a6875965e9672d5
Will Russell <will@wrussell.co.uk>
2022-12-04 21:17:13 +0000

    Day 4 solution

---
+++ day4/part2.py
@@ -0,0 +1,16 @@
+with open('input.txt') as f:
+    lines = f.readlines()
+
+    sum = 0
+    for line in lines:
+        pair = line.strip().split(",")
+        
+        first = pair[0].split("-")
+        second = pair[1].split("-")
+
+        if int(first[0]) >= int(second[0]) and int(first[0]) <= int(second[1]):
+            sum += 1
+        elif int(first[0]) <= int(second[0]) and int(first[1]) >= int(second[0]):
+            sum += 1
+        
+    print(sum)
