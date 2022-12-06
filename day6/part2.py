22408f2a6acc10ebd51f4579f47d74c2ab0a8e48
Will Russell <will@wrussell.co.uk>
2022-12-06 12:43:35 +0000

    Day 6 solution

---
+++ day6/part2.py
@@ -0,0 +1,15 @@
+with open('input.txt') as f:
+    input = f.readline()
+
+    hold = []
+    counter = 0
+    for char in input:
+        if len(hold) == 14:
+            if len(hold) == len(set(hold)):
+                break
+            else:
+                hold.pop(0)
+        hold.append(char)
+        counter += 1
+
+    print(counter)
